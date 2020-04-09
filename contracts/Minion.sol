bd756670d68901008845c20f89ea4e4c6d274d0d
nsward <wardnick@protonmail.com>
2020-04-09 17:56:28 +0400

    Update Minion to store actions

--- contracts/Minion.sol
+++ contracts/Minion.sol
@@ -4,63 +4,95 @@ import "./moloch/Moloch.sol";
 
 contract Minion {
 
-    bytes4 public constant MINION_MAGIC_BYTES = bytes4(keccak256("minion_execute"));
+    // TODO:
+    // - should we use a nonce, or just put an executed bool in the Action struct?
+        // a nonce allows us to enforce order of execution
+        // no nonce is simpler for the frontend
+    // - need to play with the details string to get something that wont break when converted to utf8
 
-    uint256 public nonce;
+    bytes4 public constant MINION_MAGIC_HASH = bytes4(keccak256("minion_action"));
+
     Moloch public moloch;
 
-    event Execute(
-        uint256 nonce,
-        bytes32 callHash
-    );
+    mapping (uint256 => Action) actions; // proposalId => Action
+
+    struct Action {
+        uint256 value;
+        address to;
+        bool executed;
+        bytes data;
+    }
+
+    event ActionProposed(uint256 _proposalId);
+    event ActionExecuted(uint256 _proposalId);
 
     constructor(address _moloch) public {
         moloch = Moloch(_moloch);
     }
 
-    // the only function the Actor can call on the parent Moloch
-    function doWithdraw(address _token, uint256 _amount) public {
-        moloch.withdrawBalance(_token, _amount);
-    }
-
-    function execute(
-        uint256 _proposalId,
-        uint256 _nonce,
-        address _to,
-        uint256 _value,
-        bytes memory _data
+    function proposeAction(
+        uint256 _sharesRequested,
+        uint256 _lootRequested,
+        uint256 _tributeOffered,
+        address _tributeToken,
+        uint256 _paymentRequested,
+        address _paymentToken,
+        address _actionTo,
+        uint256 _actionValue,
+        bytes memory _actionData
     )
         public
-        returns (bytes memory)
+        returns (uint256)
     {
-        require(_nonce == nonce, "Minion::invalid nonce");
-        nonce++;
+        // No calls to the zero address allows us to check that
+        // we submitted the proposal without getting the proposal
+        // struct from the moloch
+        require(_actionTo != address(0), "Minion::invalid _actionTo");
 
-        (
-            address applicant,,,,,,,,,,,,
-            string memory details,
-        ) = moloch.proposals(_proposalId);
+        string memory details = string(abi.encodePacked(
+            MINION_MAGIC_HASH,
+            keccak256(abi.encodePacked(_actionTo, _actionValue, _actionData))
+        ));
+
+        uint256 proposalId = moloch.submitProposal(
+            address(this),
+            _sharesRequested,
+            _lootRequested,
+            _tributeOffered,
+            _tributeToken,
+            _paymentRequested,
+            _paymentToken,
+            details
+        );
+
+        Action memory action = Action({
+            value: _actionValue,
+            to: _actionTo,
+            executed: false,
+            data: _actionData
+        });
+
+        actions[proposalId] = action;
+
+        emit ActionProposed(proposalId);
+        return proposalId;
+    }
+
+    function executeAction(uint256 _proposalId) public returns (bytes memory) {
+        Action memory action = actions[_proposalId];
         bool[6] memory flags = moloch.getProposalFlags(_proposalId);
 
-        // {4 bytes MAGIC_BYTES}{32 byte nonce}{20 byte target address}{32 byte value}{data}
-        bytes32 callHash = keccak256(abi.encodePacked(
-            MINION_MAGIC_BYTES,
-            _nonce,
-            _to,
-            _value,
-            _data
-        ));
-        bytes32 proposedCallHash = keccak256(abi.encodePacked(bytes(details)));
-
-        require(_to != address(moloch), "Minion::invalid action target");
-        require(address(this).balance >= _value, "Minion::insufficient eth");
-        require(applicant == address(this), "Minion::invalid proposal applicant");
-        require(callHash == proposedCallHash, "Minion::invalid call parameters");
+        // require(_to != address(moloch), "Minion::invalid action target");
+        require(action.to != address(0), "Minion::invalid _proposalId");
+        require(!action.executed, "Minion::action exectuted");
+        require(address(this).balance >= action.value, "Minion::insufficient eth");
         require(flags[2], "Minion::proposal not passed");
 
         // do call
-        (bool success, bytes memory retData) = _to.call.value(_value)(_data);
+        actions[_proposalId].executed = true;
+        (bool success, bytes memory retData) = action.to.call.value(action.value)(action.data);
         require(success, "Minion::call failure");
+        emit ActionExecuted(_proposalId);
         return retData;
     }
 
