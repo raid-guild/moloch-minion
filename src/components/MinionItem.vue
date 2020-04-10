6bb2b75b2a00a9173e9dd621f3e8ccf9d5323954
Dekan <dekanbro@gmail.com>
2020-04-10 16:50:13 -0600

    get rid of the red

--- src/components/MinionItem.vue
+++ src/components/MinionItem.vue
@@ -14,25 +14,6 @@
               </v-card-actions>
             </v-card>
           </v-col>
-
-          <v-col v-for="(item, i) in items" :key="i" cols="12">
-            <v-card :color="item.color" dark>
-              <div class="d-flex flex-no-wrap justify-space-between">
-                <div>
-                  <v-card-title
-                    class="headline"
-                    v-text="item.title"
-                  ></v-card-title>
-
-                  <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-                </div>
-
-                <v-avatar class="ma-3" size="125" tile>
-                  <v-img :src="item.src"></v-img>
-                </v-avatar>
-              </div>
-            </v-card>
-          </v-col>
         </v-row>
       </v-container>
     </v-card>
