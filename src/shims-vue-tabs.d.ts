b06af7057c431e1a4d4fa956eec12fb9ed7b530f
Kevin Siegler <kevinsiegler54@gmail.com>
2020-05-29 07:55:33 -0500

    feat: implement abi UI
    - parse abi for functions
    - populate function dropdown from parsed abi
    - dynamically generate parameter fields

---
+++ src/shims-vue-tabs.d.ts
@@ -0,0 +1,1 @@
+declare module "vue-nav-tabs";
--- src/views/NewMinion.vue
+++ src/views/NewMinion.vue
@@ -10,54 +10,106 @@
       </v-col>
     </v-row>
     <v-row align="center" justify="center">
-      <v-col xs="8" style="max-width: 500px">
-        <v-form ref="form" v-model="valid" lazy-validation>
-          <v-text-field
-            v-model="target"
-            label="Target Address"
-            :rules="targetRules"
-            required
-          ></v-text-field>
+      <vue-tabs>
+        <v-tab title="ABI Selector">
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="target"
+              label="Target Address"
+              :rules="targetRules"
+              required
+            ></v-text-field>
 
-          <v-text-field
-            v-model="description"
-            label="Short Description"
-            :rules="descriptionRules"
-            required
-          ></v-text-field>
+            <v-text-field
+              v-model="description"
+              label="Short Description"
+              :rules="descriptionRules"
+              required
+            ></v-text-field>
 
-          <v-textarea
-            name="input-7-1"
-            v-model="hexData"
-            filled
-            label="Data (Hex Encoded)"
-            :rules="hexDataRules"
-            auto-grow
-            required
-            value=""
-          ></v-textarea>
+            <v-textarea
+              name="input-7-1"
+              v-model="abiData"
+              filled
+              label="ABI String"
+              :rules="abiStringRules"
+              required
+              value=""
+              @input="getFunctions"
+            ></v-textarea>
+            <v-select
+              label="Functions"
+              :items="abiFunctions"
+              @change="chooseFunction"
+            ></v-select>
+            <template v-for="input in inputs">
+              <p :key="input.name">{{ input.name }} (type: {{ input.type }})</p>
+              <v-text-field :id="input.name" :key="input.id"></v-text-field>
+            </template>
+            <v-checkbox
+              v-model="checkbox"
+              label="I'm ok with this"
+              required
+            ></v-checkbox>
 
-          <v-checkbox
-            v-model="checkbox"
-            label="I'm ok with this"
-            required
-          ></v-checkbox>
+            <v-btn color="success" class="mr-4" @click="submit">
+              Submit
+            </v-btn>
+          </v-form>
+        </v-tab>
+        <v-tab title="Raw Bytes">
+          <v-col xs="8" style="max-width: 500px">
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                v-model="target"
+                label="Target Address"
+                :rules="targetRules"
+                required
+              ></v-text-field>
 
-          <v-btn color="success" class="mr-4" @click="submit">
-            Submit
-          </v-btn>
-        </v-form>
-      </v-col>
+              <v-text-field
+                v-model="description"
+                label="Short Description"
+                :rules="descriptionRules"
+                required
+              ></v-text-field>
+
+              <v-textarea
+                name="input-7-1"
+                v-model="hexData"
+                filled
+                label="Data (Hex Encoded)"
+                :rules="hexDataRules"
+                auto-grow
+                required
+                value=""
+              ></v-textarea>
+
+              <v-checkbox
+                v-model="checkbox"
+                label="I'm ok with this"
+                required
+              ></v-checkbox>
+
+              <v-btn color="success" class="mr-4" @click="submit">
+                Submit
+              </v-btn>
+            </v-form>
+          </v-col>
+        </v-tab>
+      </vue-tabs>
     </v-row>
   </v-container>
 </template>
 <script>
+import { VueTabs, VTab } from "vue-nav-tabs";
+import "vue-nav-tabs/themes/vue-tabs.css";
 export default {
   props: {
     overlay: String,
     minions: Array
   },
-  components: {},
+  components: { VueTabs, VTab },
   data: () => ({
     valid: false,
     target: "",
@@ -76,9 +128,40 @@ export default {
       v => !!v || "Hex Data is required",
       v => (v && v.startsWith("0x")) || "Must start with 0x"
     ],
+    selectedFunction: null,
+    inputs: [],
+    abiFunctions: [],
+    abiData: "",
+    abiStringRules: [
+      v => {
+        try {
+          JSON.parse(v);
+        } catch (e) {
+          return "invalid ABI string";
+        }
+        return true;
+      }
+    ],
     checkbox: false
   }),
   methods: {
+    chooseFunction(fName) {
+      const test = this.abiFunctions.find(({ name }) => name === fName);
+      this.inputs = test.inputs;
+    },
+    getFunctions(abiString) {
+      this.selectedFunction = null;
+      let abi;
+      try {
+        abi = JSON.parse(abiString);
+      } catch (e) {
+        // let validation handle errors
+        return [];
+      }
+      this.abiFunctions = abi
+        .filter(({ type, constant }) => type === "function" && !constant)
+        .map((f, id) => ({ ...f, text: f.name, id }));
+    },
     submit() {
       if (!this.valid) {
         return false;
